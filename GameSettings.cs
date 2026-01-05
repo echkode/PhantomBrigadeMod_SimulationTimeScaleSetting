01d69d18ef9e5a53b8e079954f29ccb305f7b023
EchKode <48565771+echkode@users.noreply.github.com>
2026-01-05 13:39:08 -0700

    Add toggle to game options for turn ease in/out
    At the start and end of the simulation turn, the time scale is eased into full
    speed and eased out to pause, respectively. This commits adds a toggle to
    disable that easing so the turn starts immediately at full speed and pauses
    exactly when the turn ends.
    
    Closes #3

--- GameSettings.cs
+++ GameSettings.cs
@@ -35,6 +35,13 @@ namespace EchKode.PBMods.SimulationTimeScaleSetting
             }
             implementations[keySimulationFullSpeedScaleFactor] = SimulationTimeScaleFull;
             implementations[keySimulationSlowSpeedScaleFactor] = SimulationTimeScaleSlow;
+            implementations[keySimulationTurnEaseInOut] = SimulationTurnEaseInOut;
+
+            var sim = DataShortcuts.sim;
+            timeScaleEaseOutTime = sim.timeScaleEaseOutTime;
+            timeScaleMinEaseOut = sim.timeScaleMinEaseOut;
+            timeScaleEaseOutSpeed = sim.timeScaleEaseOutSpeed;
+            timeScaleEaseInSpeed = sim.timeScaleEaseInSpeed;
         }
 
         static void SimulationTimeScaleFull(DataContainerGameSetting definition, string valueRaw)
@@ -55,8 +62,31 @@ namespace EchKode.PBMods.SimulationTimeScaleSetting
             DataShortcuts.sim.timeScaleSlow = scale;
         }
 
+        static void SimulationTurnEaseInOut(DataContainerGameSetting definition, string valueRaw)
+        {
+            var sim = DataShortcuts.sim;
+            if (!SettingUtility.TryParseBool(valueRaw))
+            {
+                sim.timeScaleEaseOutTime = -1f;
+                sim.timeScaleMinEaseOut = -1f;
+                sim.timeScaleEaseOutSpeed = 1f;
+                sim.timeScaleEaseInSpeed = 10f;
+                return;
+            }
+            sim.timeScaleEaseOutTime = timeScaleEaseOutTime;
+            sim.timeScaleMinEaseOut = timeScaleMinEaseOut;
+            sim.timeScaleEaseOutSpeed = timeScaleEaseOutSpeed;
+            sim.timeScaleEaseInSpeed = timeScaleEaseInSpeed;
+        }
+
         // This is the key of the entry in the game settings config database.
         public const string keySimulationFullSpeedScaleFactor = "game_combat_simulation_time_scale_full";
         public const string keySimulationSlowSpeedScaleFactor = "game_combat_simulation_time_scale_slow";
+        const string keySimulationTurnEaseInOut = "game_combat_simulation_turn_ease_inout";
+
+        static float timeScaleEaseOutTime;
+        static float timeScaleMinEaseOut;
+        static float timeScaleEaseOutSpeed;
+        static float timeScaleEaseInSpeed;
     }
 }
